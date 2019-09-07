4d3402a248d362b46ee02f4ebe6800a28ebbdaaf
jacknehme <jack.nehme@gmail.com>
2019-09-06 22:13:34 -0700

    Completed Module 3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Home from './Home';
 import Speakers from './Speakers'
 
+export const ConfigContext = React.createContext();
+
 const pageToShow = pageName => {
     if (pageName === 'Home') return <Home />;
     if (pageName === 'Speakers') return <Speakers />;
     return <div>{pageToShow(pageName)}</div>
 };
 
+const configValue = {
+    showSignMeUp: false,
+    showSpeakerSpeakingDays: true
+}
+
 const App = ({ pageName }) => {
-    return <div>{pageToShow(pageName)}</div>
+    return (
+        <ConfigContext.Provider value={configValue}>
+            <div>{pageToShow(pageName)}</div>
+        </ConfigContext.Provider>
+
+    )
 };
 
-export default App;
+    export default App;
